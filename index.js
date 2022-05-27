bc14af2497d9e79f2c4ba51e6bbea89a041bc635
azimuddin2 <mohammadazimuddin274@gmail.com>
2022-05-27 10:13:07 +0600

    added is order sever side code

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jwuefig.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const toolCollection = client.db('electricTools').collection('tools');
+        const orderCollection = client.db('electricTools').collection('orders');
+
+        app.get('/tool', async(req, res) => {
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+
+        app.get('/tool/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const tool = await toolCollection.findOne(query);
+            res.send(tool);
+        })
+
+        app.post('/order', async(req, res) => {
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+    }
+    finally{}
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello Electric Tools Manufacturer!')
